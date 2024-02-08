65afbcc522a4e51eaf8c6d2e19723b96765bf838
Anthony <anthonybilodeau214@gmail.com>
2024-02-08 07:28:39 -0500

    feat(loader) multiprocessing in TarDatset;

--- loader.py
+++ loader.py
@@ -1,13 +1,16 @@
 
 import os 
+import time
 import json
 import tarfile
 import numpy 
 import io
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader, get_worker_info
 import torch
 from PIL import Image
 from typing import Any
+
+from tqdm.auto import tqdm
 
 class TarFLCDataset(Dataset):
     def __init__(self, tar_path: str, use_cache: bool = False, max_cache_size: int = 16e9, image_channels: int = 3, transform: Any = None) -> None:
@@ -15,23 +18,36 @@ class TarFLCDataset(Dataset):
         self.tar_path = tar_path
         self.image_channels = image_channels
         self.transform = transform
-        with tarfile.open(tar_path) as archive:
-            self.members = archive.getmembers()
+
+        worker = get_worker_info()
+        worker = worker.id if worker else None
+        self.tar_obj = {worker: tarfile.open(self.tar_path, "r")}
+
+        # store headers of all files and folders by name
+        self.members = list(sorted(self.tar_obj[worker].getmembers(), key=lambda m: m.name))
+        self.members_by_name = {m.name: m for m in self.members}       
 
         if use_cache:
             self.__fill_cache()
 
-    def __get_item_from_tar(self, fname: str):
-        with tarfile.open(self.tar_path, "r") as archive:
-            buffer = io.BytesIO()
-            buffer.write(archive.extractfile(fname).read())
-            buffer.seek(0)
-            data = numpy.load(buffer, allow_pickle=True)
-            # TODO
-            # data will be a npz with image and metadata
-            # We should decide if we return the full npz file here or only select elements
-            # Returnning everything for now
-            return data
+    def __get_item_from_tar(self, member: tarfile.TarInfo):
+
+        # ensure a unique file handle per worker, in multiprocessing settings
+        worker = get_worker_info()
+        worker = worker.id if worker else None
+        if worker not in self.tar_obj:
+            self.tar_obj[worker] = tarfile.open(self.tar_path, "r")
+
+        buffer = io.BytesIO()
+        buffer.write(self.tar_obj[worker].extractfile(self.members_by_name[member.name]).read())
+        buffer.seek(0)
+        data = numpy.load(buffer, allow_pickle=True)
+
+        # TODO
+        # data will be a npz with image and metadata
+        # We should decide if we return the full npz file here or only select elements
+        # Returnning everything for now
+        return data
 
     def __fill_cache(self):
         pass
@@ -43,18 +59,40 @@ class TarFLCDataset(Dataset):
         if idx in self.__cache.keys():
             data = self.__cache[idx]
         else:
-            data = self.__get_item_from_tar(self.members[idx].name) # will have to be a link between the idx and the fname here
+            data = self.__get_item_from_tar(self.members[idx]) # will have to be a link between the idx and the fname here
         
         img = data["image"] # assuming 'img' key
         metadata = data["metadata"]
         
+        img = img / 255.
+        img = torch.tensor(img, dtype=torch.float32)
+
         if self.transform is not None:
             img = self.transform(img)
         return img # and whatever other metadata we like
+    
+    def __del__(self):
+        """
+        Close the TarFile file handles on exit.
+        """
+        for o in self.tar_obj.values():
+            o.close()
             
+    def __getstate__(self):
+        """
+        Serialize without the TarFile references, for multiprocessing compatibility.
+        """
+        state = dict(self.__dict__)
+        state['tar_obj'] = {}
+        return state
+
 if __name__ == "__main__":
 
     path = "/home-local2/projects/FLCDataset/dataset.tar"
     dataset = TarFLCDataset(path)
-    for i in range(10):
-        print(dataset[i])
+    dataloader = DataLoader(dataset=dataset, batch_size=128, num_workers=4)
+    import time 
+    start = time.time()
+    for i, X in enumerate(tqdm(dataloader)):
+        X
+    print(time.time() - start)
