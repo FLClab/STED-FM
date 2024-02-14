987724167d57519e4298df3b71a82bf20ccd6f32
Anthony <anthonybilodeau214@gmail.com>
2024-02-14 15:18:48 -0500

    feat(braulier) modified metadata from braulier;

--- datasets/braulier/copy-files.sh
+++ datasets/braulier/copy-files.sh
@@ -3,7 +3,7 @@
 rclone copyto "valeria-s3:flclab-archive/braulier/Flavie/TIFF files for Flavie/2-Color STED CaMKII-ACTIN/README.txt" ./readme.txt
 
 # Copies all tifffiles in Inserts
-rclone copy --progress --update --dry-run \
+rclone copy --progress --update \
     "valeria-s3:flclab-archive/braulier/Flavie/TIFF files for Flavie/2-Color STED CaMKII-ACTIN" \
     "valeria-s3:flclab-private/FLCDataset/braulier/2-Color STED CaMKII-ACTIN" \
     --filter-from filter-files.txt
