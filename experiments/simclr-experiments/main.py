1413f6d9f2677a9a2d33ed7ab5126fe5e6bfdf7d
Anthony Bilodeau <anthonybilodeau214@gmail.com>
2024-02-20 08:14:55 -0500

    feat(simclr) training for compute canada;

--- experiments/simclr-experiments/main.py
+++ experiments/simclr-experiments/main.py
@@ -4,6 +4,7 @@ import torchvision
 import numpy
 import os
 import typing
+import random
 
 from lightly import loss
 from lightly import transforms
@@ -60,10 +61,14 @@ if __name__ == "__main__":
 
     import argparse
     parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type=int, default=42,
+                    help="Random seed")     
     parser.add_argument("--restore-from", type=str, default="",
                     help="Model from which to restore from") 
-    parser.add_argument("--save-folder", type=str, default="./data/ssl/baselines",
+    parser.add_argument("--save-folder", type=str, default="./data/SSL/baselines",
                     help="Model from which to restore from")     
+    parser.add_argument("--dataset-path", type=str, default="./data/FLCDataset/20240214-dataset.tar",
+                    help="Model from which to restore from")         
     parser.add_argument("--backbone", type=str, default="resnet18",
                         help="Backbone model to load")
     parser.add_argument("--use-tensorboard", action="store_true",
@@ -73,6 +78,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    numpy.random.seed(args.seed)
+    random.seed(args.seed)
+    torch.manual_seed(args.seed)
 
     backbone = get_backbone(args.backbone)
     
@@ -123,19 +132,19 @@ if __name__ == "__main__":
     # Create a dataset from your image folder.
     manager = Manager()
     cache_system = manager.dict()
-    tar_path = "./data/FLCDataset/20240214-dataset.tar"
+    tar_path = args.dataset_path
     if args.dry_run:
         tar_path = "./data/FLCDataset/debug-dataset.tar"
     dataset = TarFLCDataset(
         tar_path=tar_path, transform=transform, 
-        use_cache=True, cache_system=cache_system, max_cache_size=8e9)
+        use_cache=True, cache_system=cache_system, max_cache_size=16e9)
 
     # Build a PyTorch dataloader.
     dataloader = torch.utils.data.DataLoader(
         dataset,  # Pass the dataset to the dataloader.
-        batch_size=32,  # A large batch size helps with the learning.
+        batch_size=128,  # A large batch size helps with the learning.
         shuffle=True,  # Shuffling is important!
-        num_workers=0
+        num_workers=4
     )
 
     # Lightly exposes building blocks such as loss functions.
