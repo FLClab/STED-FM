aa99a2a68f87d2c06558a3240f44cce71076db68
Anthony <anthonybilodeau214@gmail.com>
2024-02-22 08:09:46 -0500

    feat(notebooks) modified notebooks; feat(backbones) added naive model;

---
+++ experiments/simclr-experiments/backbones/naive.py
@@ -0,0 +1,23 @@
+
+import torch
+import numpy
+
+class NaiveModel(torch.nn.Module):
+
+    def __init__(self):
+
+        super().__init__()
+
+    def forward(self, x):
+
+        quantiles = numpy.quantile(x, q=[0., 0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.99, 1.0], axis=(1, 2, 3)).T
+        quantiles = torch.tensor(quantiles)
+
+        return quantiles
+    
+def get_backbone(name: str) -> torch.nn.Module:
+    if name == "naive":
+        backbone = NaiveModel()
+    else:
+        raise NotImplementedError(f"`{name}` not implemented")
+    return backbone
