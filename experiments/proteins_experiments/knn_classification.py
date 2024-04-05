33ba3a9f14a35988ba08e4fe64ca6e4eca8fb554
Frédéric Beaupré <fred.beaupre3@gmail.com>
2024-04-05 08:19:05 -0400

    update(proteins experiment): ProteinDataset

--- experiments/proteins_experiments/knn_classification.py
+++ experiments/proteins_experiments/knn_classification.py
@@ -7,34 +7,46 @@ from torch.utils.data import DataLoader
 from sklearn.neighbors import NearestNeighbors
 from collections import defaultdict, OrderedDict
 from utils.data_utils import load_theresa_proteins
+from models.intensity_model import get_intensity_model
 import argparse
+plt.style.use("dark_background")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--class-type", "-ct", type=str, default="protein")
 parser.add_argument("--datapath", type=str)
+parser.add_argument("--pretraining", type=str, default="STED")
 args = parser.parse_args()
 
 
-def load_model():
-    backbone = torchvision.models.resnet18()
-    backbone.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(7,7), stride=(2,2), padding=1, bias=False)
-    backbone.fc = torch.nn.Identity()
-    checkpoint = torch.load("/home/frbea320/projects/def-flavielc/baselines/resnet18/result.pt")
-    print(checkpoint.keys())
-    model_dict = checkpoint["model"]["backbone"]
-    backbone.load_state_dict(model_dict)
+def load_model(pretraining: str = "STED"):
+    if pretraining == "STED":
+        backbone = torchvision.models.resnet18()
+        backbone.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(7,7), stride=(2,2), padding=1, bias=False)
+        backbone.fc = torch.nn.Identity()
+        checkpoint = torch.load("/home/frederic/Datasets/FLCDataset/baselines/result.pt")
+        print(checkpoint.keys())
+        model_dict = checkpoint["model"]["backbone"]
+        backbone.load_state_dict(model_dict)
+    elif pretraining == "ImageNet":
+        print("Loading ResNet-18 pre-trained on ImageNet")
+        backbone = torchvision.models.resnet18(weights="IMAGENET1K_V1")
+        backbone.fc = torch.nn.Identity()
+    elif pretraining == "Intensity":
+        print("Loading IntensityModel")
+        backbone = get_intensity_model()
+    else:
+        exit(f"Model {args.pretraining} not implemented yet.")
     return backbone
 
 def knn_predict(model: torch.nn.Module, loader: DataLoader, device: torch.device):
     out = defaultdict(list)
-    for x, proteins, conditions in tqdm(loader):
-        labels = proteins if args.class_type == "protein" else conditions
-        print(labels[0], type(labels[0]))
-        x, labels = x.to(device), labels.to(device)
-        features = model(x)
-        out["features"].extend(features.cpu().data.numpy())
-        out["labels"].extend(labels.cpu().data.numpy().tolist())
-        break
+    with torch.no_grad():
+        for x, proteins, conditions in tqdm(loader, desc="Extracting features..."):
+            labels = proteins if args.class_type == "protein" else conditions
+            x, labels = x.to(device), labels.to(device)
+            features = model(x)
+            out["features"].extend(features.cpu().data.numpy())
+            out["labels"].extend(labels.cpu().data.numpy().tolist())
     samples = np.array(out['features'])
     labels = np.array([int(item) for item in out['labels']])
     neigh = NearestNeighbors(n_neighbors=6)
@@ -43,16 +55,16 @@ def knn_predict(model: torch.nn.Module, loader: DataLoader, device: torch.device
     associated_labels = labels[neighbors]
 
     uniques = np.unique(labels)
+    print(np.unique(labels, return_counts=True))
     confusion_matrix = np.zeros((len(uniques), len(uniques)))
-    for unique in uniques:
-        print(F"Unique type: {type(unique)}")
+    for unique in tqdm(uniques, desc="Confusion matrix computation..."):
         mask = labels == unique
         for predicted_unique in uniques:
-            print(F"Predicted type: {type(unique)}")
             votes = np.sum((associated_labels[mask] == predicted_unique).astype(int), axis=-1)
             confusion_matrix[unique, predicted_unique] += np.sum(votes >= 3)
     accuracy = np.diag(confusion_matrix).sum() / np.sum(confusion_matrix)
     print(f"Accuracy: {accuracy * 100:0.2f}")
+    acc = accuracy * 100
     fig, ax = plt.subplots()
     cm = confusion_matrix / np.sum(confusion_matrix, axis=-1)[np.newaxis]
     ax.imshow(cm, vmin=0, vmax=1, cmap="Purples")
@@ -65,19 +77,21 @@ def knn_predict(model: torch.nn.Module, loader: DataLoader, device: torch.device
             )
     ax.set(
         xticks=uniques, yticks=uniques,
-        xticklabels=['Round', 'Elongated', 'Multidomain'],
-        yticklabels=['Round', 'Elongated', 'Multidomain']    
+        # xticklabels=['Round', 'Elongated', 'Multidomain'],
+        # yticklabels=['Round', 'Elongated', 'Multidomain']    
     )
-    fig.savefig("./results/ResSTEDNet/knn_results.png")
+    ax.set_title(round(acc, 4))
+    fig.savefig(f"./results/{args.pretraining}/{args.class_type}_knn_results.pdf", dpi=1200, bbox_inches='tight', transparent=True)
     plt.close(fig)
 
 def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = load_model().to(device)
+    model = load_model(pretraining=args.pretraining).to(device)
     model.eval()
     loader = load_theresa_proteins(
-        path=args.datapath,
-        class_type="protein"
+        path="/home/frederic/Datasets/FLCDataset",
+        class_type=args.class_type,
+        n_channels=1 if args.pretraining == "STED" else 3,
     )
     knn_predict(model=model, loader=loader, device=device)
 
--- experiments/proteins_experiments/utils/data_utils.py
+++ experiments/proteins_experiments/utils/data_utils.py
@@ -6,12 +6,65 @@ from datasets.datasets import ProteinDataset
 PROTEINS_PATH = "/home/frbea320/scratch/Datasets/FLCDataset/TheresaProteins"
 H5SIZE = 67436
 
-def load_theresa_proteins(path: str = PROTEINS_PATH, class_ids: List = None, class_type: str = "protein", batch_size: int = 256):
+def fewshot_loader(
+        path: str = PROTEINS_PATH, 
+        class_ids: List = None,
+        batch_size: int = 256,
+        validation_size: float = 0.10,
+        class_type: str = "protein",
+        n_channels: int = 1
+        ) -> DataLoader:
+        indices = np.arange(0, H5SIZE, 1)
+        np.random.shuffle(indices)
+        split = int( (H5SIZE) * (1 - validation_size))
+        train_indices = indices[:split]
+        valid_indices = indices[split:]
+        train_dataset = ProteinDataset(
+            h5file=f"{path}/theresa_proteins.hdf5",
+            class_ids=None,
+            class_type=class_type,
+            n_channels=n_channels,
+            indices=train_indices
+        )
+        valid_dataset = ProteinDataset(
+            h5file=f"{path}/theresa_proteins.hdf5",
+            class_ids=None,
+            class_type=class_type,
+            n_channels=n_channels,
+            indices=valid_indices
+        )
+        print(f"Training size: {len(train_dataset)}")
+        print(f"Validation size: {len(valid_dataset)}")
+        train_loader = DataLoader(
+            dataset=train_dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            drop_last=False,
+            num_workers=6,
+        )
+        valid_loader = DataLoader(
+            dataset=valid_dataset,
+            batch_size=batch_size,
+            shuffle=True,
+            drop_last=False,
+            num_workers=6,
+        )
+
+        return train_loader, valid_loader
+
+def load_theresa_proteins(
+        path: str = PROTEINS_PATH, 
+        class_ids: List = None, 
+        class_type: str = "protein", 
+        batch_size: int = 256, 
+        n_channels: int = 1) -> DataLoader:
     train_dataset = ProteinDataset(
         h5file=f"{path}/theresa_proteins.hdf5",
         class_ids=None,
-        class_type=class_type
+        class_type=class_type,
+        n_channels=n_channels
     )
+    print(f"Dataset size: {len(train_dataset)}")
     train_loader = DataLoader(
         dataset=train_dataset,
         batch_size=batch_size,
