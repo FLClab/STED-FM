d4e3511bbfef7a48f56a07a233ddc6283bd448b1
Anthony <anthonybilodeau214@gmail.com>
2024-11-21 12:02:25 -0500

    feat(mitochondria) added a new segmentatio ndataset;

--- experiments/segmentation-experiments/datasets/__init__.py
+++ experiments/segmentation-experiments/datasets/__init__.py
@@ -7,6 +7,7 @@ from .fp import get_dataset as get_fp_dataset
 from .synprot import get_dataset as get_synaptic_protein_dataset
 from .lioness import get_dataset as get_lioness_dataset
 from .zooniverse import get_dataset as get_zooniverse_dataset
+from .mitochondria import get_dataset as get_mitochondria_dataset
 
 DATASETS = {
     "factin" : get_actin_dataset,
@@ -17,7 +18,8 @@ DATASETS = {
     "synaptic-semantic-segmentation" : get_synaptic_protein_dataset,
     "perforated-segmentation" : get_synaptic_protein_dataset,
     "multidomain-detection" : get_synaptic_protein_dataset,
-    "lioness" : get_lioness_dataset
+    "lioness" : get_lioness_dataset,
+    "mitochondria" : get_mitochondria_dataset,
 }
 
 def get_dataset(name: str, cfg: dataclass, **kwargs) -> Dataset:
