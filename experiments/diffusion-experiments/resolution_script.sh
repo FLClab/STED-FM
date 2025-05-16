354fd9628f15d7e561486463e9265a1cd62898b6
Frédéric Beaupré <fred.beaupre3@gmail.com>
2025-05-16 09:45:00 -0400

    update(codebase): some more code cleaning

--- experiments/diffusion-experiments/resolution_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python resolution_experiment.py --weights MAE_SMALL_STED
-python resolution_experiment.py --weights MAE_SMALL_SIM 
-python resolution_experiment.py --weights MAE_SMALL_HPA
-python resolution_experiment.py --weights MAE_SMALL_JUMP
-python resolution_experiment.py --weights MAE_SMALL_IMAGENET1K_V1
